1f6202ac2a2bf326923913302e5975b9890e9f53
oktavianrs <oktavianrs@gmail.com>
2016-08-15 16:22:49 +0200

    Step info request fixed data

--- app/components/stepTableGenerator/stepTableGenerator.js
+++ app/components/stepTableGenerator/stepTableGenerator.js
@@ -20,26 +20,24 @@ angular.module('components.stepTableGenerator', [])
 
     	// Interaction with model and data
 
-        $scope.getTableType = function() {
-            stepModel.getTableType(function(res) {
-            	$scope.tableType = res;
-            });
-        }
-        $scope.getTableType();
-
-        $scope.getTableParams = function() {
-            stepModel.getTableParams(function(res) {
-            	$scope.tableParams = res;
-            });
-        }
-        $scope.getTableParams();
-
         $scope.sendTableData = function() {
             $scope.datas = {tables: [$scope.datas]};
             stepModel.sendTableData($scope.stateParams.eventId, $scope.datas, function(data) {
+                if(data.model) {
+                    // $scope.stepperCtrl.steps.model.step += 1;
+                }
                 $scope.datas = {};
         	});
         }
+
+        $scope.getStepInfo = function() {
+            stepModel.getStepInfo($scope.stateParams.eventId,function(data) {
+                $scope.stepInfo = data;
+                $scope.tableType = data.additional_value.type;
+                $scope.tableParams = data.additional_value.param; 
+            })
+        }
+        $scope.getStepInfo();
 
 
     }])
